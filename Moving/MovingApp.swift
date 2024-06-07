a8b8442a0fb65f0a3b46fa6c98a250956cd996f6
Michelle Thorn <Michelle@michellethorn.net>
2024-06-06 19:14:54 -0700

    Initial Commit

---
+++ Moving/MovingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovingApp.swift
+//  Moving
+//
+//  Created by Michelle Thorn on 6/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MovingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
